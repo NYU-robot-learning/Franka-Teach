c1be26fa1b6e812a1e0f6cede7a710e50adcaf39
BobbyPeng123 <zp2019@nyu.edu>
2025-10-03 15:34:47 -0400

    update ip

--- frankateach/constants.py
+++ frankateach/constants.py
@@ -29,7 +29,7 @@ STATE_TOPIC = "state"
 CONTROL_TOPIC = "control"
 
 # VR constants
-VR_TCP_HOST = "10.19.182.138"
+VR_TCP_HOST = "10.21.47.240"
 VR_TCP_PORT = 5555
 VR_CONTROLLER_TOPIC = b"oculus_controller"
 
